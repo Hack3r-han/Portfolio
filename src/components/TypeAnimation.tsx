196bba036ac2d9a4bb16ec33181d536adc489880
Hack3r-han <hannahphil27@gmail.com>
2024-03-29 11:51:48 +0100

    Make the type animation more responsive

--- src/components/TypeAnimation.tsx
+++ src/components/TypeAnimation.tsx
@@ -1,7 +1,10 @@
 import { TypeAnimation } from 'react-type-animation';
+import { useMediaQuery } from 'react-responsive';
 import ComputerScreen from '../assets/Desktop.png'; 
 
 const WelcomeMessage = () => {
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+
   return (
     <div
       style={{
@@ -13,36 +16,38 @@ const WelcomeMessage = () => {
         alignItems: 'center',
       }}
     >
-      <div
-        style={{
-          position: 'absolute',
-          top: '45%', 
-          left: '20%', 
-          textAlign: 'center', 
-          width: '50%',
-          fontFamily: 'Courier New, monospace',
-          fontSize: '1.5em', 
-          fontWeight: 'bold', 
-          color: '#32CD32',
-        }}
-      >
-        <TypeAnimation
-          sequence={[
-            'Hello..',
-            1000,
-            'World!',
-            2000,
-            'Mundo!',
-            2000,
-            'Monde!',
-            2000
-          ]}
-          wrapper="span"
-          speed={60}
-          style={{ display: 'inline-block' }}
-          repeat={Infinity}
-        />
-      </div>
+      {!isMobile ? (
+        <div
+          style={{
+            position: 'absolute',
+            top: '45%', 
+            left: '20%', 
+            textAlign: 'center', 
+            width: '50%',
+            fontFamily: 'Courier New, monospace',
+            fontSize: '1.5em', 
+            fontWeight: 'bold', 
+            color: '#32CD32',
+          }}
+        >
+          <TypeAnimation
+            sequence={[
+              'Hello..',
+              1000,
+              'World!',
+              2000,
+              'Mundo!',
+              2000,
+              'Monde!',
+              2000
+            ]}
+            wrapper="span"
+            speed={60}
+            style={{ display: 'inline-block' }}
+            repeat={Infinity}
+          />
+        </div>
+      ) : null}
       <img
         src={ComputerScreen}
         alt="Computer Screen"
