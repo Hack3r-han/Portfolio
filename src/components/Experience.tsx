713fce9c60927f00958bcd2a197e4782233e90f3
Hannah Phillips <145985469+Hack3r-han@users.noreply.github.com>
2025-07-18 18:21:45 +0200

    Update Experience.tsx
    Edit copy so it reflects current working situation

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -18,15 +18,15 @@ const Experience = () => {
       {/* EDUCATION */}
       <div className="lg:w-1/2 p-8">
         <div className="py-6 lg:py-12 flex flex-col gap-4">
-          <p className="text-sm tracking-[4px] font-mono text-orange-400">2014 - 2024</p>
+          <p className="text-sm tracking-[4px] font-mono text-orange-400">2014 - 2025</p>
           <h2 className="text-3xl md:text-4xl font-bold font-mono text-sky-600">Education</h2>
         </div>
         <div className="mt-6 lg:mt-14 w-full h-[1000px] border-l-[6px] border-l-sky-600 border-opacity-30 flex flex-col gap-10">
           <ResumeCard
             title="Full Stack Web Development"
-            subTitle="Factoría F5 (Oct - present)"
+            subTitle="Factoría F5 (Oct - May)"
             place="MADRID"
-            des="A comprehensive 6-month bootcamp which covered the latest skills in web development, by tackling front-end and back-end projects."
+            des="A comprehensive 8-month bootcamp which covered the latest skills in web development, by tackling front-end and back-end projects."
           />
           <ResumeCard
             title="Google IT Automation with Python (112 hrs)"
@@ -38,7 +38,7 @@ const Experience = () => {
             title="BA Foreign Languages, Literatures, and Linguistics"
             subTitle="University of Liverpool (2016 - 2020)"
             place="2:1"
-            des="Specialized in French. Through immersive study, cultural exploration, and linguistic analysis, I developed a deep understanding of the intricacies of  language."
+            des="Specialised in French and Spanish. Through immersive study, cultural exploration, and linguistic analysis, I developed a deep understanding of the intricacies of  language."
           />
         </div>
       </div>
@@ -46,7 +46,7 @@ const Experience = () => {
       {/* EXPERIENCE */}
       <div className="lg:w-1/2 p-8">
         <div className="py-6 lg:py-12 flex flex-col gap-4">
-          <p className="text-sm tracking-[4px] font-mono text-orange-400">2020 - 2024</p>
+          <p className="text-sm tracking-[4px] font-mono text-orange-400">2020 - 2025</p>
           <h2 className="text-3xl md:text-4xl font-bold font-mono text-sky-600">Job Experience</h2>
         </div>
         <div className="mt-6 lg:mt-14 w-full h-[1000px] border-l-[6px] border-l-sky-600 border-opacity-30 flex flex-col gap-10">
@@ -66,7 +66,7 @@ const Experience = () => {
           <ResumeCard
             title="English Teaching Assistant"
             subTitle="IES Vicente Aleixandre (2020 - 2021)"
-            place="SEVILLA"
+            place="SEVILLE"
             des="Delivered interactive classes in a bilingual college in Seville, spanning a variety of themes, including History, Music, Biology and Technology."
           />
         </div>
